b77237377faeb16907b1798391da41b019b170ef
phantomaton <182378863+phantomaton-ai@users.noreply.github.com>
2025-04-22 21:09:01 +0000

    test

---
+++ phantomaton-browser.test.js
@@ -0,0 +1,53 @@
+import { expect, stub } from 'lovecraft';
+
+import phantomaton from 'phantomaton';
+import conversations from 'phantomaton-conversations';
+import plugins from 'phantomaton-plugins';
+import puppeteer from 'puppeteer';
+import browser from './phantomaton-browser.js';
+
+const mockPage = () => ({
+  click: stub().resolves(undefined),
+  close: stub().resolves(undefined),
+  goto: stub().resolves(undefined),
+  hover: stub().resolves(undefined),
+  type: stub().resolves(undefined)
+});
+
+describe('Phantomaton Browser', () => {
+  let mockConverse;
+  let mockPages;
+  let mockNewPage;
+  let assistant;
+
+  beforeEach(async () => {
+    mockConverse = stub().resolves('Okay...');
+    mockPages = [mockPage(), mockPage()];
+    mockNewPage = mockPage();
+    stub(puppeteer, 'launch').resolves({
+      newPage: stub().resolves(mockNewPage),
+      pages: stub().resolves(mockPages)
+    });
+    assistant = await phantomaton('Testing', { install: [
+      'phantomaton-conversations',
+      'phantomaton-execution',
+      browser({}),
+      plugins.create([
+        plugins.define(conversation.assistant).as({
+          converse: mockConverse
+        }),
+        plugins.define(plugins.start).with([
+          conversation.assistant
+        ]).as((assistant) => () => assistant)
+      ])
+    ] });
+  });
+
+  afterEach(() => {
+    puppeteer.launch.restore();
+  });
+
+  it('launches puppeteer', () => {
+    expect(puppeteer.launch.calledOnce).to.eq(true);
+  });
+});
